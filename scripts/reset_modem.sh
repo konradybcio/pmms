97cc7ca39769e173068fc250a492ff819c496ff5
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-04-10 23:08:30 +0200

    scripts/reset_modem.sh: Refactor (with futureproofing)

--- scripts/reset_modem.sh
+++ scripts/reset_modem.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
 # eg25-manager claims the gpios
-# the script assumes a manjaro CE PP
 sudo systemctl stop eg25-manager || sudo service eg25-manager stop
 
-GPIO=/sys/class/gpio
-PIN=/sys/class/gpio/gpio
-PC4_RESET=68
+PINEMODEL=$((cat /proc/device-tree/model) | cut -c18-22)
 
-printf $PC4_RESET > $GPIO/export
+PINE10NAME="(1.0)"
+PINE11NAME="(1.1)"
+PINE12NAME="(1.2)"
 
-printf out > $PIN$PC4_RESET/direction
+[ $PINEMODEL == $PINE10NAME ] && PC4_RESET=68
+[ $PINEMODEL == $PINE11NAME ] && PC4_RESET=68
+[ $PINEMODEL == $PINE12NAME ] && PC4_RESET=68
 
-printf 1 > $PIN$PC4_RESET/value
+echo "Detected PinePhone version $PINEMODEL, using pin $PC4_RESET as reset"
+
+# If PC4_RESET is not exported, export it
+[ ! -d /sys/class/gpio/gpio$PC4_RESET ] && printf $PC4_RESET > /sys/class/gpio/export
+
+printf out > /sys/class/gpio/gpio$PC4_RESET/direction
+
+printf 1 > /sys/class/gpio/gpio$PC4_RESET/value
 sleep 0.5
-printf 0 > $PIN$PC4_RESET/value
+printf 0 > /sys/class/gpio/gpio$PC4_RESET/value
