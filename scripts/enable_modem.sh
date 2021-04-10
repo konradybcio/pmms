941c2679a0883e8b786cec07f4d9f0dd6b02f890
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-04-10 23:17:11 +0200

    scripts/enable_modem.sh: Refactor (with futureproofing)

--- scripts/enable_modem.sh
+++ scripts/enable_modem.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
 
-GPIO=/sys/class/gpio
-PIN=/sys/class/gpio/gpio
-PB3_PWRKEY=35
-#359
-PL7_PWR=34
-PC4_RESET=68
-PH8_DISABLE=232
-PH7_WAKEUP=231
+PINEMODEL=$((cat /proc/device-tree/model) | cut -c18-22)
 
-printf $PL7_PWR > $GPIO/export
-printf $PC4_RESET > $GPIO/export
-printf $PH7_WAKEUP > $GPIO/export
-printf $PH8_DISABLE > $GPIO/export
-printf $PB3_PWRKEY > $GPIO/export
+PINE10NAME="(1.0)"
+PINE11NAME="(1.1)"
+PINE12NAME="(1.2)"
 
-printf out > $PIN$PH8_DISABLE/direction
-printf out > $PIN$PH7_WAKEUP/direction
-printf out > $PIN$PC4_RESET/direction
-printf out > $PIN$PL7_PWR/direction
-printf out > $PIN$PB3_PWRKEY/direction
+[ $PINEMODEL == $PINE10NAME ] && PC4_RESET=68 && PB3_PWRKEY=35 && PH7_WAKEUP=231 && PH8_DISABLE=232 && PL7_PWR=358
+[ $PINEMODEL == $PINE11NAME ] && PC4_RESET=68 && PB3_PWRKEY=35 && PH7_WAKEUP=231 && PH8_DISABLE=232 && PL7_PWR=358
+[ $PINEMODEL == $PINE12NAME ] && PC4_RESET=68 && PB3_PWRKEY=35 && PH7_WAKEUP=231 && PH8_DISABLE=232 && PL7_PWR=34
 
-printf 0 > $PIN$PH8_DISABLE/value
-printf 0 > $PIN$PH7_WAKEUP/value
-printf 0 > $PIN$PC4_RESET/value
+echo "Detected PinePhone version $PINEMODEL"
+echo "Using pin $PC4_RESET as reset"
+echo "Using pin $PB3_PWRKEY as pwrkey"
+echo "Using pin $PH7_WAKEUP as wakeup"
+echo "Using pin $PH8_DISABLE as disable"
 
-printf 1 > $PIN$PL7_PWR/value
+# Export GPIOs if needed
+[ ! -d /sys/class/gpio/gpio$PL7_PWR ] && printf $PL7_PWR > /sys/class/gpio/export
+[ ! -d /sys/class/gpio/gpio$PC4_RESET ] && printf $PC4_RESET > /sys/class/gpio/export
+[ ! -d /sys/class/gpio/gpio$PH7_WAKEUP ] && printf $PH7_WAKEUP > /sys/class/gpio/export
+[ ! -d /sys/class/gpio/gpio$PH8_DISABLE ] && printf $PH8_DISABLE > /sys/class/gpio/export
+[ ! -d /sys/class/gpio/gpio$PB3_PWRKEY ] && printf $PB3_PWRKEY > /sys/class/gpio/export
+
+printf out > /sys/class/gpio/gpio$PH8_DISABLE/direction
+printf out > /sys/class/gpio/gpio$PH7_WAKEUP/direction
+printf out > /sys/class/gpio/gpio$PC4_RESET/direction
+printf out > /sys/class/gpio/gpio$PL7_PWR/direction
+printf out > /sys/class/gpio/gpio$PB3_PWRKEY/direction
+
+printf 0 > /sys/class/gpio/gpio$PH8_DISABLE/value
+printf 0 > /sys/class/gpio/gpio$PH7_WAKEUP/value
+printf 0 > /sys/class/gpio/gpio$PC4_RESET/value
+
+printf 1 > /sys/class/gpio/gpio$PL7_PWR/value
 sleep 0.1
-printf 1 > $PIN$PB3_PWRKEY/value
+printf 1 > /sys/class/gpio/gpio$PB3_PWRKEY/value
 sleep 0.2
-printf 0 > $PIN$PB3_PWRKEY/value
+printf 0 > /sys/class/gpio/gpio$PB3_PWRKEY/value
