ecaa35cc7678a3112a004c666cfb31bba7b11361
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-04-10 22:01:45 +0200

    Add a dumb setup script for ALARM

---
+++ setup_arch.sh
@@ -0,0 +1,11 @@
+# TODO: Unlock the modem automagically
+
+# make, gcc - (duh)
+# dtc - required for mkbootimg
+# python-pip - pulls in all things python nicely
+# android-tools - provides fastboot
+# usbutils - lsusb
+# inetutils - telnet
+# dhcpcd - dhcpcd :)
+
+sudo pacman -S gcc make dtc python-pip android-tools usbutils inetutils dhcpcd
