65dd1b8d9e1ed79d423840b731a37562cec828da
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-04-11 00:09:04 +0200

    Automate firstrun

--- firstrun.sh
+++ firstrun.sh
@@ -1,3 +1,8 @@
+touch .firstrun_done
+
+ALARM=$(sed '3q;d' /etc/os-release | cut -c4-100)
+[ $ALARM == "archarm" ] && echo "Detected Arch Linux ARM" && ./scripts/setup_arch.sh
+
 cd tools/mkbootimg
 make
 
--- qletsgo.sh
+++ qletsgo.sh
@@ -1,1 +1,3 @@
+[ ! -f .firstrun_done ] && ./firstrun.sh
+
 sudo ./scripts/reset_modem.sh && ./scripts/create_image.sh && sudo fastboot boot boot.img && sleep 33 && ./scripts/telnet.sh
