b0a9656910f17cbc6d9449c4983354f6f8e5527b
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-03-11 21:05:50 +0100

    firstrun.sh: Rid building mkbootimg

--- firstrun.sh
+++ firstrun.sh
@@ -2,7 +2,3 @@ touch .firstrun_done
 
 ALARM=$(sed '3q;d' /etc/os-release | cut -c4-100)
 [ $ALARM == "archarm" ] && echo "Detected Arch Linux ARM" && ./scripts/setup_arch.sh
-
-cd tools/mkbootimg
-make
-
