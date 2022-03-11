2a29586562064c8a1e69b647c3641a59a519d55e
Konrad Dybcio <konrad.dybcio@somainline.org>
2022-03-11 21:43:05 +0100

    scripts/create_image: Adjust for py3 mkbootimg

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -1,9 +1,14 @@
 ./scripts/dtbTool --version=3 -o dt.img dtb && \
-./tools/mkbootimg/mkbootimg \
+python3 ./tools/mkbootimg \
+--header_version 2 \
 --kernel zImage \
 --ramdisk ./prebuilts/initrd-debug \
---base 0x80000000 \
---dt dt.img \
--o boot.img \
+--dtb dt.img \
 --cmdline "PMOS_NO_OUTPUT_REDIRECT rootfstype=ramfs root=/dev/ram0" \
---tags-addr 0x81E00000
+--base 0x80000000 \
+--kernel_offset 0x8000 \
+--ramdisk_offset 0x1600000 \
+--dtb_offset 0x2000000 \
+--tags_offset 0x1f00000 \
+--pagesize 2048 \
+-o boot.img
